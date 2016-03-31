61be38e8c2d5be5954ef79e5f84307f4eccad569
liedkeb <liedkeb@gmail.com>
2016-03-31 15:09:33 +0200

    first commit of projects including tests

---
+++ EnterpriseMVVM.Windows/ObservableObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace EnterpriseMVVM.Windows
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler!=null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Tests/ObservableObjectTests.cs
@@ -0,0 +1,46 @@
+﻿using EnterpriseMVVM.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+
+    [TestClass]
+    class ObservableObjectTests
+    {
+        [TestMethod]
+        public void PropertyChangedEventHandlerIsRised()
+        {
+            var obj = new StubObservableObject();
+
+            bool raised = false;
+            obj.PropertyChanged += (sender, e) =>
+            {
+                Assert.IsTrue(e.PropertyName == "ChangedProperty");
+                raised = true;
+            };
+
+            obj.ChangedProperty = "Some Value";
+
+            if (!raised) Assert.Fail("PropertyChanged was never invoded.");
+        }
+    }
+
+    class StubObservableObject : ObservableObject
+    {
+        private string changedProperty;
+        public string ChangedProperty
+        {
+            get { return changedProperty; }
+            set
+            {
+                changedProperty = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
