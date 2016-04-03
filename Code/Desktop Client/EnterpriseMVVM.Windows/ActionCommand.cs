e5d3ba823b4824ff3862b59536bd7fb9fc213cee
Bartosz Liedke <liedkeb@gmail.com>
2016-04-03 13:51:19 +0200

    Finished implementing ActionCommand

---
+++ Code/Desktop Client/EnterpriseMVVM.Windows/ActionCommand.cs
@@ -0,0 +1,59 @@
+﻿
+namespace EnterpriseMVVM.Windows
+{
+    using System;
+    using System.Windows.Input;
+    public class ActionCommand: ICommand
+    {
+
+        private readonly Action<Object> action;
+        private readonly Predicate<Object> predicate;
+
+        public ActionCommand(Action<Object> action): this(action, null)
+        {
+        }
+
+        public ActionCommand(Action<Object> action, Predicate<Object> predicate)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException("action", "You must specify and Action<T>.");
+            }
+            this.action = action;
+            this.predicate = predicate;
+
+        }
+
+
+        #region ICommand implementation
+
+        public bool CanExecute(object parameter)
+        {
+            if (predicate == null)
+                return true;
+
+            return predicate(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+
+
+        public void Execute()
+        {
+            Execute(null);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/EnterpriseMVVM.Windows.Tests/ActionCommandTests.cs
@@ -0,0 +1,73 @@
+﻿namespace EnterpriseMVVM.WindowsTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using Windows;
+    [TestClass]
+    public class ActionCommandTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorThrowsExceptionIfActionParameterIsNull()
+        {
+            var command = new ActionCommand(null);
+        }
+
+        [TestMethod]
+        public void ExecuteInvokesAction()
+        {
+            var invoked = false;
+            Action<Object> action = obj => invoked = true;
+
+            var command = new ActionCommand(action);
+
+            command.Execute();
+
+            Assert.IsTrue(invoked);
+        }
+
+        [TestMethod]
+        public void ExecudeOverloadInvokeActionWithParameter()
+        {
+            var invoked = false;
+            Action<Object> action = obj =>
+            {
+                Assert.IsNotNull(obj);
+                invoked = true;
+            };
+
+            var command = new ActionCommand(action);
+
+            command.Execute(new object());
+
+            Assert.IsTrue(invoked); 
+        }
+
+        [TestMethod]
+        public void CanExecuteIsTrueByDefault()
+        {
+            var command = new ActionCommand(obj => { });
+
+            Assert.IsTrue(command.CanExecute(null));
+
+        }
+
+        [TestMethod]
+        public void CanExecuteOverloadExecutesTruePredicate()
+        {
+            var command = new ActionCommand(obj => { }, obj => (int)obj == 1);
+
+            Assert.IsTrue(command.CanExecute(1));
+
+        }
+
+        [TestMethod]
+        public void CanExecuteOverloadExecutesFalsePredicate()
+        {
+            var command = new ActionCommand(obj => { }, obj => (int)obj == 1);
+
+            Assert.IsFalse(command.CanExecute(0));
+
+        }
+    }
+}
